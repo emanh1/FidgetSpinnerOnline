853399b13ea1dc1d40509c98dfc001d091fd2da4
EmanhMC <emanhmc@gmail.com>
2017-12-03 12:34:06 +0700

    Update spinner.php

--- spinner.php
+++ spinner.php
@@ -10,19 +10,19 @@
         <meta property="og:type" content="website">
         <meta property="og:image:width" content="1200">
         <meta property="og:image:height" content="630">
-        <link href="http://emanhmc.ezyro.com/includes/css/spinner.css" media="all" rel="stylesheet">
+        <link href="http://emanhmc.ezyro.com/static/css/spinner.css" media="all" rel="stylesheet">
     </head>
     <body>
         <div class="bg"></div>
         <div class="everything">
             <div class="holder">
                 <div class="spinner">
-                    <img src="http://emanhmc.ezyro.com/includes/img/spinner.png">
+                    <img src="http://emanhmc.ezyro.com/static/img/spinner.png">
                 </div>
                 <div class="handle">
                 </div>
             </div>
         </div>
-        <script src="http://emanhmc.ezyro.com/includes/js/spinnerjs.js"></script>
+        <script src="http://emanhmc.ezyro.com/static/js/spinnerjs.js"></script>
     </body>
 </html>
