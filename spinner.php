1aa115dbb2960990dccb9ed3797937ea87126465
EmanhMC <emanhmc@gmail.com>
2018-08-09 09:07:41 +0700

    Update spinner.php

--- spinner.php
+++ spinner.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>EmanhMC - Fidget Spinner Online</title>
+        <title>Fidget Spinner Online</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width,user-scalable=no,minimum-scale=1,maximum-scale=1">
         <meta name="description" content="Fidget Spinner Online">
@@ -10,19 +10,19 @@
         <meta property="og:type" content="website">
         <meta property="og:image:width" content="1200">
         <meta property="og:image:height" content="630">
-        <link href="http://emanhmc.ezyro.com/static/css/spinner.css" media="all" rel="stylesheet">
+        <link href="spinner.css" media="all" rel="stylesheet">
     </head>
     <body>
         <div class="bg"></div>
         <div class="everything">
             <div class="holder">
                 <div class="spinner">
-                    <img src="http://emanhmc.ezyro.com/static/img/spinner.png">
+                    <img src="spinner.png">
                 </div>
                 <div class="handle">
                 </div>
             </div>
         </div>
-        <script src="http://emanhmc.ezyro.com/static/js/spinnerjs.js"></script>
+        <script src="spinnerjs.js"></script>
     </body>
 </html>
